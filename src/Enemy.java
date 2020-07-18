28f92d2a9a11a37e6e6703761fd205f95f9f58f4
William Ho <william.ho3@ucalgary.ca>
2020-07-18 14:48:31 -0600

    Updated combatstatus in player

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
 
-public class Enemy {
+public class Enemy extends Entity {
+	
+	private boolean isDead;
+	private int givesXP;
+	private ArrayList<Integer> moveSet = new ArrayList<Integer>();
+
+	public boolean isDead() {
+		return isDead;
+	}
+
+	public void setDead(boolean isDead) {
+		this.isDead = isDead;
+	}
+
+	public int getGivesXP() {
+		return givesXP;
+	}
+
+	public void setGivesXP(int givesXP) {
+		this.givesXP = givesXP;
+	}
+
+	public ArrayList<Integer> getMoveSet() {
+		return moveSet;
+	}
+
+	public void setMoveSet(ArrayList<Integer> moveSet) {
+		this.moveSet = moveSet;
+	}
+	
+	
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,30 @@
 
-public class Player {
+public class Player extends Entity {
+
+	private int xp;
+
+	public int getXp() {
+		return xp;
+	}
+
+	public void setXp(int xp) {
+		this.xp = xp;
+	}
+	
+	public void moveUp(int amount) {
+		setYcoord(getYcoord() + amount);
+	}
+	
+	public void moveDown(int amount) {
+		setYcoord(getYcoord() - amount);
+	}
+	
+	public void moveLeft(int amount) {
+		setXcoord(getXcoord() - amount);
+	}
+	
+	public void moveRight(int amount) {
+		setXcoord(getXcoord() + amount);
+	}
 
 }
