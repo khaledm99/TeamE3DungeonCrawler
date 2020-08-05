fbd817a3ee272e82db625a46b29178152b8ca91a
howat93 <68035489+howat93@users.noreply.github.com>
2020-08-05 15:16:42 -0600

    Update UserInterfaceController.java
    corrected issues

--- src/application/UserInterfaceController.java
+++ src/application/UserInterfaceController.java
@@ -1,122 +1,109 @@
-/*
 package application;
+
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import model.Entity;
+import javafx.scene.layout.AnchorPane;
 import model.Player;
 import model.Userinterface;
 
 public class UserInterfaceController extends GameController {
 
-
 	private String currentPlayerId; 
 	
 	private Player currentPlayer;
 	
-	private int currentPlayerCoinsLabel;
+	private int currentPlayerCoins;
 	
-	private int currentPlayerHealthLabel;
+	private int currentPlayerHealth;
 	
-	private double currentPlayerXPLabel;
+	private double currentPlayerXP;
 	
-	private int currentPlayerKillCountLabel;
+	private int currentPlayerKillCount;
 	
-	private int currentPlayerLevelLabel;
+	private int currentPlayerLevel;
 	
-	private Userinterface gameUI = new Userinterface(currentPlayer);
-
-	IntegerProperty coins = new SimpleIntegerProperty(this.currentPlayerCoinsLabel);
-	IntegerProperty health = new SimpleIntegerProperty(this.currentPlayerHealthLabel);
-	IntegerProperty killCount = new SimpleIntegerProperty(this.currentPlayerKillCountLabel);
-	IntegerProperty playerLvl = new SimpleIntegerProperty(this.currentPlayerLevelLabel);
-	DoubleProperty xpLabel = new SimpleDoubleProperty(currentPlayerXPLabel);
+	private Userinterface gameUI;
 	
-	private String healthBar = gameUI.healthBarStringCreator();
-	  
-		@FXML
-	    private Label healthBarLabel;
+	private String healthBar;
+	
+	IntegerProperty coins = new SimpleIntegerProperty(this.currentPlayerCoins);
+	IntegerProperty health = new SimpleIntegerProperty(this.currentPlayerHealth);
+	IntegerProperty killCount = new SimpleIntegerProperty(this.currentPlayerKillCount);
+	IntegerProperty playerLvl = new SimpleIntegerProperty(this.currentPlayerLevel);
+	DoubleProperty xpLabel = new SimpleDoubleProperty(currentPlayerXP);
+	
+	
+	
+    @FXML
+    private Label healthBarLabel;
 
-	    @FXML
-	    private Label playerKillcount;
+    @FXML
+    private Label playerKillcount;
 
-	    @FXML
-	    private Label playerCoins;
+    @FXML
+    private Label playerCoins;
 
-	    @FXML
-	    private Label playerHealth;
+    @FXML
+    private Label playerHealth;
 
-	    @FXML
-	    private Label killCountLabel;
+    @FXML
+    private Label killCountLabel;
 
-	    @FXML
-	    private Label playerXPLabel;
+    @FXML
+    private Label playerXPLabel;
 
-	    @FXML
-	    private Label playerNameLabel;
+    @FXML
+    private Label playerNameLabel;
 
-	    @FXML
-	    private ProgressBar toggleXP;
+    @FXML
+    private ProgressBar toggleXP;
 
-	    @FXML
-	    private Label playerLvlLabel;
+    @FXML
+    private AnchorPane statsWindow;
 
-	    @FXML
-	    private Label playerXP;
+    @FXML
+    private Label playerLvlLabel;
 
-	    @FXML
-	    private Label PlayerLvl;
+    @FXML
+    private Label playerXP;
 
-	    @FXML
-	    private Label playerCoinsLabel;
+    @FXML
+    private Label PlayerLvl;
 
-	    @FXML
-	    private Label playerID;
+    @FXML
+    private Label playerCoinsLabel;
 
-	    @FXML
-	    void toggleONProgress(ActionEvent event) {
+    @FXML
+    private Label playerID;
 
-	    }
+    @FXML
+    void toggleONProgress(ActionEvent event) {
 
-	    @FXML
-	    void toggleOFFProgress(ActionEvent event) {
-
-	    }
-    
-    
-    
-    public UserInterfaceController(Player gamePlayer, Entity currentEntity) {
-		this.currentPlayerCoinsLabel = gamePlayer.getCoins();
-		this.currentPlayerKillCountLabel = gamePlayer.getKillCount();
-		this.currentPlayerXPLabel = gamePlayer.getXp();
-		this.currentPlayerHealthLabel = gamePlayer.getHp();
-		this.currentPlayerLevelLabel = gamePlayer.getLevel();
-		this.currentPlayer = gamePlayer;
-	}
-    
-    public void setTextLabelPlayerKillcount(int currentKillCount){
-    	
     }
-    
+
+    @FXML
+    void toggleOFFProgress(ActionEvent event) {
+
+    }
     
 	@FXML 
 	void initialize() {
-		
+	
 		playerNameLabel.setText(this.currentPlayerId);
-		killCountLabel.setText(Integer.toString(this.currentPlayerKillCountLabel));
-		playerCoinsLabel.setText(Integer.toString(this.currentPlayerCoinsLabel));
-		playerLvlLabel.setText(Integer.toString(this.currentPlayerLevelLabel));
-		healthBarLabel.setText(replaceHealthBarCharacters(healthBar));
-		playerXPLabel.setText(Double.toString(this.currentPlayerXPLabel));
+		killCountLabel.setText(Integer.toString(this.currentPlayerKillCount));
+		playerCoinsLabel.setText(Integer.toString(this.currentPlayerCoins));
+		playerLvlLabel.setText(Integer.toString(this.currentPlayerLevel));
+		//healthBarLabel.setText(this.healthBar);
+		playerXPLabel.setText(Double.toString(this.currentPlayerXP));
 		 
 	}
-	
+    
 	private String replaceHealthBarCharacters (String healthBarString) {
 		
 		char[] currentHealthBar = healthBarString.toCharArray();
@@ -130,31 +117,38 @@ public class UserInterfaceController extends GameController {
 		
 		String heartHealthBar = String.valueOf(currentHealthBar);
 		
-		return heartHealthBar;
-		
+		return heartHealthBar;	
 		
 	}
 	
+	public void uiInitializer(Player currentPlayer) {
+
+			this.currentPlayerCoins = currentPlayer.getCoins();
+			this.currentPlayerKillCount = currentPlayer.getKillCount();
+			this.currentPlayerXP = currentPlayer.getXp();
+			this.currentPlayerHealth = currentPlayer.getHp();
+			this.currentPlayerLevel = currentPlayer.getLevel();
+			this.currentPlayer = currentPlayer;
+			this.gameUI = new Userinterface(currentPlayer);
+			this.healthBar = this.gameUI.healthBarStringCreator();
 	
+	    }
 	
+
 	@Override
 	public void refresh() {
+		
 		// TODO Auto-generated method stub
 		
-		this.currentPlayer= getPlayer();
+		uiInitializer(getPlayer());
+		
+		//Where to bind property bindings?
 		playerNameLabel.setText(this.currentPlayerId);
 		killCountLabel.textProperty().bind(killCount.asString());
 		playerCoinsLabel.textProperty().bind(coins.asString());
 		playerLvlLabel.textProperty().bind(playerLvl.asString());
 		playerXPLabel.textProperty().bind(xpLabel.asString());
-		
-		
-		
-	
+		healthBarLabel.setText(replaceHealthBarCharacters(this.healthBar));
 	}
-	
-	
-	
-}
 
-*/
+}
